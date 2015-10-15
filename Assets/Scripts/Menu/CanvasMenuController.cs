8da7593e6ad12f6d96d757ed04721fc3c1135e07
Alessio Boerio <boeriozap@hotmail.it>
2015-10-15 20:08:05 +0200

    Menu is linked to the battle scene; set the first button for joypad navigation

---
+++ Assets/Scripts/Menu/CanvasMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasMenuController : MonoBehaviour
+{
+	public Button startingButton;
+
+	private EventSystem eventSystem;
+
+
+	void Start ()
+	{
+		this.eventSystem = EventSystem.current;
+		this.eventSystem.firstSelectedGameObject = this.startingButton.gameObject;//SetSelectedGameObject(this.startingButton, new BaseEventData(this.eventSystem));
+	}
+}
