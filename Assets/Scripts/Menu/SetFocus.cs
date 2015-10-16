43af14ff1e8f2694f4aeaa36c8014d897888b409
Alessio Boerio <boeriozap@hotmail.it>
2015-10-16 12:01:25 +0200

    Fix menu joypad navigation for draw status

---
+++ Assets/Scripts/Menu/SetFocus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SetFocus : MonoBehaviour
+{
+	public GameObject objectFocused;
+	private EventSystem eventSystem;
+	
+	void OnEnable ()
+	{
+		Debug.Log ("Enabling");
+		this.eventSystem = EventSystem.current;
+		this.eventSystem.SetSelectedGameObject(this.objectFocused);
+	}
+}
